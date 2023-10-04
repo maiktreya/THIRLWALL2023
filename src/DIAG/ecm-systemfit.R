3d41496965621d842d48c48ce18a1003572e8b1f
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-04 17:19:44 +0200

    last

--- src/DIAG/ecm-systemfit.R
+++ src/DIAG/ecm-systemfit.R
@@ -11,10 +11,9 @@ uecm_systemfit <- function(
     method = "SUR",
     method_solv = "EViews",
     iterations = 1,
-    table = data.table::data.table()) {
+    dt = data.table::data.table()) {
     diff_cols <- c()
     all_lag_cols <- c()
-    dt <- table
 
     for (col in col_names) {
         # Add diff column
@@ -81,13 +80,15 @@ recm_systemfit <- function(
     method_solv = "EViews",
     iterations = 1,
     nlags = 1,
-    table = data.table::data.table()) {
+    dt = data.table::data.table()) {
     diff_cols <- c()
     all_lag_cols <- c()
-    dt <- table
 
     # get and incorporate ECT from UECM
-    ect_test <- get_ect_systemfit(systemfit_uecm_coefs = uecm_model, sel_variables = col_names)
+    ect_test <- get_ect_systemfit(
+        systemfit_uecm_coefs = uecm_model,
+        sel_variables = col_names
+    )
     dt <- cbind(dt, ect_test)
     ect <- dt$ect_test
 
@@ -139,23 +140,28 @@ recm_systemfit <- function(
 # Define dataset of usage (data.table required) and selected variables for coint. analysis. The dependant var should be listed first.
 table_dt <- fread("Data/.CSV/COMTRADE/eudata_final_nom.csv")[reporter == "Spain" & tech == "HIGH"]
 sel_variables <- c("tech_exports", "fincome", "rprices")
+lags <- 2
+iterations <- 1
 
 # Get an Unrestricted ECM using systemfit methods
 pre_exp <- uecm_systemfit(
-    table = table_dt,
+    dt = table_dt,
     col_names = sel_variables,
-    nlags = 2,
+    nlags = lags,
     method = "SUR",
-    iterations = 1
+    iterations = iterations,
+    method_solv = "EViews" # only 3sls
 )
 
 # Get a restricted ECM using systemfit methods
 pos_exp <- recm_systemfit(
-    table = table_dt,
     uecm_model = pre_exp,
+    dt = table_dt,
     col_names = sel_variables,
+    nlags = lags,
     method = "SUR",
-    iterations = 1
+    iterations = iterations,
+    method_solv = "EViews" # only 3sls
 ) %>%
     summary() %>%
     print()
