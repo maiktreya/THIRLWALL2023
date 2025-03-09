b64aa09ac461101c8f41f3c3d9a8a32f188f85a9
maiktreya <miguel.garcia.duch@gmail.com>
2025-03-09 02:20:31 +0100

    update march

--- src/API-current/test_comtradr.R
+++ src/API-current/test_comtradr.R
@@ -1,31 +1,40 @@
+library(magrittr)
+library(data.table)
 library(comtradr)
-library(curl)
-# Ensure the curl package is installed
-if (!requireNamespace("curl", quietly = TRUE)) {
-    install.packages("curl")
+
+
+set_primary_comtrade_key("a22118ffda8c47339b31151f2d935095")
+
+lista_paises <- ct_get_ref_table("reporter") %>% as.data.table()
+clas_sitc4 <- ct_get_ref_table("S4") %>% as.data.table()
+
+# Query goods data for China's trade with Argentina and Germany in 2019
+active <- TRUE
+if (active) {
+    get_trade_data <- function(reporters, commodity_codes) {
+        Sys.sleep(1)
+        ct_get_data(
+            type = "goods",
+            commodity_classification = "HS",
+            commodity_code = commodity_codes,
+            reporter = reporters,
+            partner = c("ARG", "DEU"),
+            start_date = "2019",
+            end_date = "2019",
+            flow_direction = "Import",
+            partner_2 = "World",
+            verbose = TRUE
+        )
+    }
 }
 
-# Set the primary COMTRADE key
-Sys.setenv("COMTRADE_PRIMARY" = "a22118ffda8c47339b31151f2d935095")
-set_primary_comtrade_key()
+example <- get_trade_data("CHN", "TOTAL") %>% as.data.table()
 
-ct_get_data_with_traceback <- function(...) {
-    tryCatch(
-        {
-            ct_get_data(...)
-        },
-        error = function(e) {
-            traceback()
-            stop(e)
-        }
-    )
-}
 
-ct_get_data_with_traceback(
-    commodity_code = "everything",
-    reporter = "CHN",
-    partner = "DEU",
-    start_date = "2019",
-    end_date = "2019",
-    flow_direction = "Import"
-)
+example_sum <- example %>%
+    .[, .(sum = sum(as.numeric(primary_value), na.rm = TRUE)),
+        by = .(period, reporter_code)
+    ]
+
+print(example)
+print(example_sum)
